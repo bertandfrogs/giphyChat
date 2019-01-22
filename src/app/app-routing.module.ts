adc4b695647ed070049470220e4aaaea08b1b25d
Sarah Smalley <bertandfrogs@gmail.com>
2019-01-22 09:33:32 -0700

    added routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthLoginComponent} from './auth-login/auth-login.component';
+import {GiphyTitlesComponent} from './giphy-titles/giphy-titles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: 'login',
+      component: AuthLoginComponent
+    },
+    {
+      path: 'titles',
+      component: GiphyTitlesComponent
+    },
+    { path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
