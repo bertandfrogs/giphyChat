a5716b25e649c5c35b7b6178a9ac60815d4b7d36
Sarah Smalley <bertandfrogs@gmail.com>
2019-01-22 09:03:18 -0700

    created auth and giphytitles components

---
+++ src/app/giphy-titles/giphy-titles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy-titles',
+  templateUrl: './giphy-titles.component.html',
+  styleUrls: ['./giphy-titles.component.css']
+})
+export class GiphyTitlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
