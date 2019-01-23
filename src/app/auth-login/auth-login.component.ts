6b1fb8f6a2ab012465dc95dab2b9a1f6bd15597d
Ricardo Baeza <baezaric3600@gmail.com>
2019-01-23 10:09:38 -0700

    fixed the email display issue

--- src/app/auth-login/auth-login.component.ts
+++ src/app/auth-login/auth-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {AuthLoginService} from "./auth-login.service";
 import {CurrentUserData} from "../current-user-data";
 
@@ -10,7 +10,8 @@ import {CurrentUserData} from "../current-user-data";
 export class AuthLoginComponent implements OnInit {
   x = new CurrentUserData("", {});
   userId;
-  constructor(private db: AuthLoginService) { }
+  constructor(private db: AuthLoginService,
+              private zone: NgZone) { }
 
   ngOnInit() {
 
@@ -18,8 +19,10 @@ export class AuthLoginComponent implements OnInit {
 
   logIn () {
     this.db.logIn().then( data => {
-      this.userId= data.user.email;
-      console.log(this.userId);
+      this.zone.run(()=>{
+        this.userId= data.user.email;
+        console.log(this.userId);
+      });
     } );
   }
 
