5ad1f6b0f0a6326c22eff104d966f4d6343ceb83
Sarah Smalley <bertandfrogs@gmail.com>
2019-01-24 10:23:11 -0700

    added chat add and chat delete functionality

--- src/app/giphy-titles/giphy-titles.component.ts
+++ src/app/giphy-titles/giphy-titles.component.ts
@@ -8,17 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class GiphyTitlesComponent implements OnInit {
   chatList = [];
   noContentInList = true;
+  deleteToggle = false;
+  id=0;
   constructor() { }
 
   ngOnInit() {
   }
 
   addChat(){
-    this.chatList.push({name: "Chat Name"});
+    this.unDelete();
+    this.chatList.push({name: "Chat Name", id: this.id});
     this.noContentInList = false;
+    this.id++;
+
   }
 
   deleteChat() {
-      console.log("delete chat");
+      this.deleteToggle = true;
+  }
+
+  unDelete() {
+      this.deleteToggle = false;
+  }
+
+  deleteThis(id) {
+    for(let i = 0; i < this.chatList.length; i++){
+      if(this.chatList[i].id == id){
+        this.chatList.splice(i, 1);
+      }
+    }
+    if(this.chatList[0] === null){
+        this.noContentInList = true;
+    }
   }
 }
